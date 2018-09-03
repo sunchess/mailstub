797d86d8a6647ed7efb5a2f194ae67f7158208cb
AlexanderDmitriev <alexanderdmv@gmail.com>
2018-09-03 12:13:46 +0300

    WIP: added attacments to parse email

--- lib/mail_to_json/mail_parser.ex
+++ lib/mail_to_json/mail_parser.ex
@@ -6,13 +6,18 @@ defmodule MailToJson.MailParser do
     parse_mail_bodies(body)
     |> Map.merge extract_mail_meta(mail_meta)
   end
-
   def parse_mail_data({"image" = content_type_name, content_subtype_name, mail_meta, _, body}) do
-    #создать таблицу attachments и туда сохранять {"Content-ID", "<5b7570f526e14_88393fdd7ec3ee0c2273d@MacBook-Pro-Dmitriev.local.mail>"} сам файл сохранять в файловую систему откуда показывать
-    # <img src=\"cid:5b7570f526e14_88393fdd7ec3ee0c2273d@MacBook-Pro-Dmitriev.local.mail\" />
-
+    result = %{"body" => body, "meta" => mail_meta}
+    %{image: result}
   end
-
+  def parse_mail_data({"application" = content_type_name, content_subtype_name, mail_meta, _, body}) do
+    result = %{"body" => body, "meta" => mail_meta}
+    %{application: result}
+  end
+  def parse_mail_data({"text", "plain", [_, _, _, {"Content-ID", _}] = mail_meta, _, body}) do
+    result = %{"body" => body, "meta" => mail_meta}
+    %{file: result}
+  end
   def parse_mail_data({"text" = content_type_name, content_subtype_name, mail_meta, _, body})
     when content_subtype_name == "plain" or content_subtype_name == "html" do
 
@@ -27,8 +32,14 @@ defmodule MailToJson.MailParser do
 
   defp parse_mail_bodies([], collected), do: collected
 
-  defp parse_mail_bodies([body | bodies], collected \\ %{}) do
-    new_collected = Map.merge(collected, parse_mail_data(body))
+  defp parse_mail_bodies([body | bodies], collected \\ %{"applications" => [], "images" => [], "files" => []}) do
+    parsed = case parse_mail_data(body) do
+      %{application: application} -> %{"applications" => collected["applications"] ++ [ application ]}
+      %{image: image}             -> %{"images" => collected["images"] ++ [ image ]}
+      %{file: image}              -> %{"files" => collected["files"] ++ [ image ]}
+      mail_part -> mail_part
+    end
+    new_collected = Map.merge(collected, parsed)
     parse_mail_bodies(bodies, new_collected)
   end
 
